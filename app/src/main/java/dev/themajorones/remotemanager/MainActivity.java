f03bf73722d7008badf72428c9922980dab213ea
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2025-07-10 15:21:34 +0700

    portrait and landscape layout

--- app/src/main/java/dev/themajorones/remotemanager/MainActivity.java
+++ app/src/main/java/dev/themajorones/remotemanager/MainActivity.java
@@ -3,10 +3,13 @@ package dev.themajorones.remotemanager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.window.layout.FoldingFeature;
+import dev.themajorones.remotemanager.adapter.DeviceAdapter;
+import dev.themajorones.remotemanager.entity.Device;
 import dev.themajorones.remotemanager.utils.Preload;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +24,25 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         Preload.load(this);
         setContentView(R.layout.activity_main);
+        testFragment();
+    }
+
+    private void testFragment() {
+        Device d1 = new Device().setHost("Device 1");
+        Device d2 = new Device().setHost("Device 2");
+        Device d3 = new Device().setHost("Device 3");
+        Device d4 = new Device().setHost("Device 4");
+        Device d5 = new Device().setHost("Device 5");
+        Device d6 = new Device().setHost("Device 6");
+        Device d7 = new Device().setHost("Device 7");
+        Device d8 = new Device().setHost("Device 8");
+        Device d9 = new Device().setHost("Device 9");
+        Device d10 = new Device().setHost("Device 10");
+
+        ListView listView = findViewById(R.id.listView);
+
+        Device[] devices = {d1, d2, d3, d4, d5, d6, d7, d8, d9, d10};
+        DeviceAdapter adapter = new DeviceAdapter(this, java.util.Arrays.asList(devices));
+        listView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/dev/themajorones/remotemanager/adapter/DeviceAdapter.java
@@ -0,0 +1,47 @@
+package dev.themajorones.remotemanager.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.widget.ArrayAdapter;
+import dev.themajorones.remotemanager.R;
+import dev.themajorones.remotemanager.entity.Device;
+
+import java.util.List;
+
+public class DeviceAdapter extends ArrayAdapter<Device> {
+
+    public DeviceAdapter(@NonNull Context context, @NonNull List<Device> devices) {
+        super(context, 0, devices);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.main_view_device_fragment, parent, false);
+        }
+
+        Device device = getItem(position);
+
+        if (device != null) {
+            ImageView logoImageView = convertView.findViewById(R.id.logoImageView);
+            TextView deviceInfoTextView = convertView.findViewById(R.id.deviceInfoTextView);
+            Button actionButton = convertView.findViewById(R.id.actionButton);
+            Button editButton = convertView.findViewById(R.id.editButton);
+
+            deviceInfoTextView.setText(device.getHost());
+            actionButton.setOnClickListener(v -> Toast.makeText(getContext(), "Action for " + device.getHost(), Toast.LENGTH_SHORT).show());
+            editButton.setOnClickListener(v -> Toast.makeText(getContext(), "Edit for " + device.getHost(), Toast.LENGTH_SHORT).show());
+        }
+
+        return convertView;
+    }
+}
